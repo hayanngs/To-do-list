f53495a45dda9073458a91a705fea4e85379f3a0
hayannow@gmail.com <hayannow@gmail.com>
2021-08-27 19:38:41 -0300

    Adicionando o checked

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 var container_input = document.getElementById('container_input');
-var input_text = document.getElementById('input_text');
-input_text.addEventListener('focus', inputFocus);
-input_text.addEventListener('blur', inputBlur);
 
-var input_btn = document.getElementById('input_btn');
-input_btn.addEventListener('click', inputBtnClick);
+var input_text = document.getElementById('input_text');
+
+input_text.addEventListener('focus', function() {
+    container_input.classList.add("container_input_focus");
+});
+
+input_text.addEventListener('blur', function() {
+    container_input.classList.remove("container_input_focus");
+});
 
 var container_itens = document.getElementById('container_itens');
 var itens = [];
+var input_btn = document.getElementById('input_btn');
 
-function inputFocus() {
-    container_input.classList.add("container_input_focus");
-}
-
-function inputBlur() {
-    container_input.classList.remove("container_input_focus");
-}
-
-function inputBtnClick() {
-    var item = document.createElement('span');
-    item.classList.add("item");
-    item.innerText = input_text.value;
-    itens.push(item);
-    container_itens.append(item);
-}
+input_btn.addEventListener('click', function() {
+    var text = input_text.value;
+    if (text.length > 0) {
+        var item = document.createElement('span');
+        item.classList.add("item");
+        item.innerText = text;
+        input_text.value = "";
+        itens.push(item);
+        container_itens.append(item);
+        input_text.focus();
+    }
+    else {
+        //alerta campo vazio
+    }
+});
